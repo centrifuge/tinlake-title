354d74e8b7eb0194cea78db4ef5d7e468c2e7d29
Lucas Vogelsang <l@lucasvo.com>
2019-12-29 13:14:53 +0000

    Removing uint2str

--- src/title.sol
+++ src/title.sol
@@ -35,24 +35,6 @@ contract Title is ERC721Metadata {
         count = 0;
     }
 
-    // --- Utils ---
-    function uint2str(uint i) internal pure returns (string memory) {
-        if (i == 0) return "0";
-        uint j = i;
-        uint length;
-        while (j != 0){
-            length++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(length);
-        uint k = length - 1;
-        while (i != 0){
-            bstr[k--] = byte(uint8(48 + i % 10));
-            i /= 10;
-        }
-        return string(bstr);
-    }
-
     // --- Title ---
     function issue (address usr) public auth returns (uint) {
         _mint(usr, count);
