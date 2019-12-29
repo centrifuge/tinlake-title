f3c832194a1dea83026710ef5b57f10249d2f8e9
Lucas Vogelsang <l@lucasvo.com>
2019-12-29 13:13:26 +0000

    Description

--- src/title.sol
+++ src/title.sol
@@ -1,4 +1,4 @@
-// title.sol -- NFT that represents the access rights to interact with a certain loan
+// title.sol -- NFT to manage access rights to contracts
 // Copyright (C) 2019 lucasvo
 
 // This program is free software: you can redistribute it and/or modify
